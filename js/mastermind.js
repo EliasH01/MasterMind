ce7b0f60df80739e84c1f705e848f60c8475368c
eliash01 <eliashenne@gmail.com>
2017-01-12 12:49:09 +0100

    git

--- js/mastermind.js
+++ js/mastermind.js
@@ -8,12 +8,19 @@ function getColor() {
 
 var game = []
 var rounds = 12;
+var startTime = 0;
+var timer;
 
 function init() {
     game = [getColor(),getColor(),getColor(),getColor()];
     $('.mm-result').fadeOut()
    // $('.mm-result').remove() 
     $('div[id^="chooser-"]').removeClass().addClass('bullet')
+    $('.initial-hidden').hide()
+    startTime = 0;
+    window.clearInterval(timer)
+
+
 }
 
 function check() {
@@ -27,6 +34,14 @@ function check() {
     })
 
     if (result.length < 4) return;
+
+    if (timer == undefined) {
+        timer = window.setInterval(function(){
+            $('#time').text(++startTime)
+        }, 1000)
+    }
+
+    $('#rounds-display').text(rounds)
     var test = game.slice();
     var clr = 0;
     var clrpos = 0;
@@ -47,6 +62,18 @@ function check() {
     }
 
     console.log("Schwarze Möppen:" , clrpos, ", Weiße Möppen: ", clr)
+
+    rounds--
+    if (rounds == 0 && clrpos < 4) {
+        // Verloren
+        $('#loose').show()
+        window.clearInterval(timer)
+    } else if (clrpos == 4) {
+        // Gewonnen!
+        $('#win').show()
+        window.clearInterval(timer)
+    }
+
     var tableRow = '<tr class="mm-result"><td><div class="bullet '+result[0]+'"> </div></td><td><div class="bullet '+result[1]+'"> </div></td><td><div class="bullet '+result[2]+'"> </div></td><td><div class="bullet '+result[3]+'"> </div></td> ';
     for (var i = 0; i < 4; i++){
         var resClass = '';
@@ -76,7 +103,7 @@ $(document).ready(function() {
     $('.logo').click(init)
     $('div[id^="overlay-"]').each(function(idx, elem) {
         $('#chooser-' + (idx + 1)).click(function(){
-            $('#overlay-' + (idx + 1)).slideDown(200)
+            $('#overlay-' + (idx + 1)).slideDown(150)
         })
     })
     init()
